c9f67f0b9372d3a3a331c136137ec77e340ce649
Matti Hänninen <matti@mjhanninen.com>
2017-01-15 10:10:00 +0200

    Add POST for creating new event

--- src/app.js
+++ src/app.js
@@ -16,6 +16,10 @@ export var app = new Vue({
             description: "",
             date: ""
         },
+        newEventState: {
+            isOkay: true,
+            message: ""
+        },
         events: []
     },
 
@@ -25,29 +29,59 @@ export var app = new Vue({
 
     methods: {
 
-        fetchEvents: function() {
-            console.log("Requesting data");
-            this.$http
-                .get(API_EVENTS_URL)
-                .then((data) => {
-                    console.log(data);
-                    for (var event of data.body) {
-                        console.log("Pushing an event");
-                        this.events.push(event);
-                    }
-                }, (err) => {
-                    console.log(err)
-                });
+        resetEvents: function(newEvents) {
+            this.events = newEvents || [];
         },
 
-        addEvent: function() {
+        fetchEvents: function() {
+            this.$http
+                .get(API_EVENTS_URL)
+                .then((response) => {
+                    console.log(response);
+                    return response.json();
+                }, (response) => {
+                    // Error
+                    console.log(response)
+                })
+                .then(this.resetEvents);
+        },
+
+        addEvent: function(newEvent) {
+            console.log("foo");
+            if (newEvent) {
+                this.events.push(newEvent);
+            }
+        },
+
+        fail: function(message) {
+            this.newEventState.isOkay = false;
+            this.newEventState.message = message;
+        },
+
+        submitNewEvent: function() {
             if (this.event.name) {
-                this.events.push(this.event);
-                this.event = {
-                    name: "",
-                    description: "",
-                    console: ""
-                }
+                var newEvent = this.event;
+                this.$http
+                    .post(API_EVENTS_URL, newEvent)
+                    .then((response) => {
+                        console.log(response);
+                        this.event = {
+                            name: "",
+                            description: "",
+                            console: ""
+                        };
+                        this.newEventState.isOkay = true;
+                        return response.json();
+                    }, (response) => {
+                        console.log(response);
+                        this.fail("The server responded with status code " + response.status +
+                                  ". Please resubmit your event again in couple minutes.");
+                    })
+                    .then(this.addEvent);
+            }
+            else {
+                this.fail("The event is missing a name. Please provide a name and " +
+                          "then resubmit.");
             }
         },
 
